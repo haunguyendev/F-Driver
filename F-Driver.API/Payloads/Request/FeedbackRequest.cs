ff78304bf53f0e3d761757fd28f57a84aa22ce12
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-10-11 17:55:58 +0700

    [fea] api create feedbacks

---
+++ F-Driver.API/Payloads/Request/FeedbackRequest.cs
@@ -0,0 +1,37 @@
+﻿using F_Driver.Service.BusinessModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace F_Driver.API.Payloads.Request
+{
+    public class FeedbackRequest
+    {
+        [Required(ErrorMessage = "MatchId is required.")]
+        public int MatchId { get; set; }
+
+        [Required(ErrorMessage = "DriverId is required.")]
+        public int DriverId { get; set; }
+
+        [Required(ErrorMessage = "PassengerId is required.")]
+        public int PassengerId { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int Rating { get; set; }  // Đánh giá từ 1 đến 5
+
+        [MaxLength(500, ErrorMessage = "Comment must not exceed 500 characters.")]
+        public string? Comment { get; set; }
+
+
+        public FeedbackCreateModel MapToModel()
+        {
+            return new FeedbackCreateModel
+            {
+                MatchId = this.MatchId,
+                DriverId = this.DriverId,
+                PassengerId = this.PassengerId,
+                Rating = this.Rating,
+                Comment = this.Comment,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
+    }
+}
---
+++ F-Driver.Service/BusinessModels/FeedbackCreateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F_Driver.Service.BusinessModels
+{
+    public class FeedbackCreateModel
+    {
+        public int MatchId { get; set; }
+        public int DriverId { get; set; }
+        public int PassengerId { get; set; }
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
